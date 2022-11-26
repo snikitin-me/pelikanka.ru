00cf1caa9a3b970c1036ebca327ac2bb4333526e
Sergey Nikitin <snikitin.me@gmail.com>
2022-11-26 18:39:54 +0300

    Fix blogSidebarTitle

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,7 @@ const config = {
         // },
         blog: {
           showReadingTime: true,
+          blogSidebarTitle: 'Все посты',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
