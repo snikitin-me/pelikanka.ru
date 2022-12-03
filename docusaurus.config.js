38282db43a345cdfdef041cd699981cb38c3abb2
Sergey Nikitin <snikitin.me@gmail.com>
2022-12-03 22:28:41 +0300

    To the Moon!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
         // },
         blog: {
           showReadingTime: true,
-          blogSidebarTitle: 'Все посты',
+          blogSidebarTitle: 'Выпуски',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
--- src/pages/components/Services.jsx
+++ src/pages/components/Services.jsx
@@ -21,11 +21,11 @@ export default function SolveProblem(props) {
             <div class="accordion">
                 <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                     Трудности в поведение ребёнка
                     </button>
                 </h2>
-                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
                         «Плохим» поведением ребенок пытается сообщить взрослым о какой-то проблеме, которая требует решения. Моя задача помочь родителям понять 
                         истинные причины возникновения у ребенка трудностей в поведении, определить вызвано ли данное поведение возрастным кризисом и дать рекомендации, 
