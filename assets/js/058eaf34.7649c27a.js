c40d4101ebabe6465726fe4cac08070f00593811
Sergey Nikitin <snikitin.me@gmail.com>
2023-02-01 21:40:46 +0300

    Deploy website - based on 2b338f9a2c339257673c27bf06334dc508ed848b

---
+++ assets/js/058eaf34.7649c27a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpelikanka_docu_boot=self.webpackChunkpelikanka_docu_boot||[]).push([[2962],{4225:(e,t,n)=>{n.r(t),n.d(t,{default:()=>c});var o=n(7294);class c extends o.Component{constructor(e){super(e)}componentDidMount(){}render(){return o.createElement("section",{id:"PageFooter",class:"row gx-4 justify-content-center"})}}}}]);
